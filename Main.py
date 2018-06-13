66b24236620e88290d65c3103c93d84c1c80c9e0
keithliam <keithliamm@gmail.com>
2018-06-13 15:45:55 +0800

    Add choices printer functions

---
+++ Main.py
@@ -0,0 +1,14 @@
+def printRoadChoices():
+	print('[1] EDSA')
+	pirnt('[2] Commonwealth')
+	print('[3] Quezon Avenue')
+	print('[4] España')
+	print('[5] C5')
+	print('[6] Ortigas')
+	print('[7] Marcos Highway')
+	print('[8] Roxas Blvd.')
+	print('[9] SLEX')
+
+def printDirectionChoices():
+	print('[1] Northbound')
+	print('[2] Southbound')
