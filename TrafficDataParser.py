179096b0ce39a8b84a929b97aab7a7ef2b2c6ab8
keithliam <keithliamm@gmail.com>
2018-06-13 16:16:10 +0800

    Add files date parser

--- TrafficDataParser.py
+++ TrafficDataParser.py
@@ -1,5 +1,17 @@
+from os.path import isfile
+from os import listdir
+
 class TrafficDataParser:
-	def __init__(self, road, direction, path):
+	def __init__(self, road, direction):
 		self.road = road
 		self.direction = direction
 		self.path = path
+
+	def parse(self):
+		csvString = '30 Minutes,Lane 1 Flow (Veh/30 Minutes),# Lane Points,% Observed'
+
+		filesFolders = listdir(self.path)
+
+		for fileFolder in filesFolders:
+			if isfile(self.path + fileFolder):
+				csvString += '\n' + parseDate(fileFolder[15:23])
